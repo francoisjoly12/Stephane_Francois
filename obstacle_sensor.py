c03ca8fd4f9b2df3db5ff472a53a799d1ca7cb18
stefprovost <199664102@cegepsherbrooke.qc.ca>
2024-04-26 12:58:27 -0400

    gaz_sensor et obstacle_sensor ok

--- obstacle_sensor.py
+++ obstacle_sensor.py
@@ -12,17 +12,19 @@ class ObstacleSensor:
     def obstacle_detected(self):
         return not self.sensor.value
 
-    def wait_for_obstacle(self):
-        while not self.obstacle_detected():
-            time.sleep(0.1)
-
 def main():
     obstacle_sensor = ObstacleSensor(board.IO16)  # Utilisation de la broche D11
+    
+    
     try:
+        time1 = time.monotonic()
+
         while True:
-            if obstacle_sensor.obstacle_detected():
-                print("Barrier detected!")
-                obstacle_sensor.wait_for_obstacle()  # Attendez que l'obstacle soit retiré avant de continuer
+            if(time.monotonic() - time1 > 1):
+                time1 = time.monotonic()
+                if obstacle_sensor.obstacle_detected():
+                    print("Barrier detected!")
+                    
     except KeyboardInterrupt:
         pass  # Ignorez l'exception KeyboardInterrupt et terminez le programme proprement
 
