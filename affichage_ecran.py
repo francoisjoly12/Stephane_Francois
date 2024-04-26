62bd147d5aaafd1371383bc1dee2b33e64d3e96f
stefprovost <199664102@cegepsherbrooke.qc.ca>
2024-04-26 15:17:03 -0400

    affichage_écran

---
+++ affichage_ecran.py
@@ -0,0 +1,41 @@
+import board
+import displayio
+import terminalio
+from adafruit_display_text import label
+import adafruit_displayio_ssd1306
+import time
+
+class Ecran:
+
+    def __init__(self):
+        displayio.release_displays()
+        self.i2c = board.I2C()
+        self.display_bus = displayio.I2CDisplay(self.i2c, device_address=0x3C)
+        self.display = adafruit_displayio_ssd1306.SSD1306(self.display_bus, width=128, height=64, rotation=180)
+        self.splash = displayio.Group()
+        self.display.root_group = self.splash
+
+        self.text = ""
+        self.text_area = label.Label(terminalio.FONT, text=self.text, color=0xFFFFFF, x=5, y=10)
+        self.splash.append(self.text_area)
+
+    def refresh_text(self, humidity, gas_level, door_state, fan_state):
+        self.text_area.text = "Humidité: {:.1f}%\nNiveau Gaz: {:.1f}ppm\nPorte: {}\nVentillateur: {}".format(
+                    humidity, gas_level, door_state, fan_state)
+        self.display.refresh()
+
+
+last_display_time = time.monotonic()
+last_measurement_time = time.monotonic()
+humidity = 0
+gas_level = 0
+door_state = "Ouvert"
+fan_state = "Ventillation"
+ecran = Ecran()
+
+while True:
+    current_time = time.monotonic()
+    if current_time - last_display_time > 0.5:
+        last_display_time = current_time
+
+    ecran.refresh_text(humidity, gas_level, door_state, fan_state)
